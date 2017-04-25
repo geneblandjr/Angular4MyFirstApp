878fbf31d7ca2951016ae6bbdd810072f5fefd09
Gene Bland <geneblandjr@gmail.com>
2017-04-25 15:03:52 -0400

    Input Binding and Data Output of Input

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
    allowNewServer = false;
    serverCreationStatus = 'No Server Was Created.';
+   serverName = '';
   constructor() {
    setTimeout(() => {
        this.allowNewServer = true;
@@ -19,5 +20,8 @@ export class ServersComponent implements OnInit {
 
     onCreateServer(){
         this.serverCreationStatus = 'Server was Created!';
+    }
+    OnUpdateServerName(event: Event) {
+        this.serverName = (<HTMLInputElement>event.target).value;
     }
 }
